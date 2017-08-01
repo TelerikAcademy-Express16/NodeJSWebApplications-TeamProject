0f50230b731f2b91ec27087663749920ba91402e
theshaftman <mariyan_apostolov@mail.bg>
2017-08-01 08:07:29 +0300

    Initialize Web Socket #4

--- TicketInformationSystem/server/server.js
+++ TicketInformationSystem/server/server.js
@@ -5,6 +5,7 @@ const async = () => {
 };
 
 const config = require("../config");
+let io = require("socket.io");
 
 const run = () => {
     return async()
@@ -12,12 +13,34 @@ const run = () => {
         .then((db) => require("../data").init(db))
         .then((data) => require("../application").init(data))
         .then((app) => {
+            // io = io(app);
+            // io.on("connection", (client) => {
+            // });
+            
+            // io.sockets.on('connection', (client) => {
+            //     console.log('io.connection called');
+            //     client.on("event", (data) => {});
+            //     client.on('updateData', (json, action, id) => {
+            //         if (action == "edit" || action == "create") {
+            //             console.log('updateData event');
+            //         }
+            //         updateData(client, json, action, id);
+            //     } );
+            //     client.on("disconnect", () => {});
+            // });
             app.listen(config.port, () =>
                 console.log(`Server listening to port: ${config.port}`));
         })
         .catch((err) => {
             console.log(err);
         });
+}
+
+function updateData(socket, json, action, id) {
+    if (action == "edit" || action == "create") {
+        console.log('updateData called');
+    }
+    socket.broadcast.to('clients').emit('updateData', json, action, id);
 }
 
 module.exports = { run };
